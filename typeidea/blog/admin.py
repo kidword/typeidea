bd77b09b6864bba69e5daf64f5ea3f3661735804
xbc <1594636097@qq.com>
2019-07-20 21:30:55 +0800

    修改admin后台管理

--- typeidea/blog/admin.py
+++ typeidea/blog/admin.py
@@ -1,3 +1,63 @@
 from django.contrib import admin
+from .models import Post, Category, Tag
+from django.urls import reverse
+from django.utils.html import format_html
+
 
 # Register your models here.
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'status', 'is_nav', 'created_time')
+    fields = ('name', 'status', 'is_nav', 'owner')
+
+    def save_model(self, request, obj, form, change):
+        obj.owner = request.user
+        return super(CategoryAdmin, self).save_model(request, obj, form, change)
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name', 'status', 'created_time')
+    fields = ('name', 'status')
+
+    def save_model(self, request, obj, form, change):
+        obj.owner = request.user
+        return super(TagAdmin, self).save_model(request, obj, form, change)
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'category', 'status', 'created_time', 'operator']
+    list_display_links = []
+
+    list_filter = ['category', ]
+    search_fields = ['title', 'category__name']
+
+    actions_on_top = True
+    actions_on_bottom = True
+
+    # 编辑页面
+    save_on_top = True
+
+    fields = (
+        ('category', 'title'),
+        'desc',
+        'status',
+        'content',
+        'tag'
+    )
+
+    def operator(self, obj):
+        return format_html('<a href="{}">编辑</a>',
+                           reverse('admin:blog_post_change', args=(obj.id)))
+
+    operator.short_description = '操作'
+
+    def save_model(self, request, obj, form, change):
+        obj.owner = request.user
+        return super(PostAdmin, self).save_model(request, obj, form, change)
+
+
+
