420e62c885f7da4e53e95bd616452cee9f135f11
xbc <1594636097@qq.com>
2019-07-21 10:12:42 +0800

    修改post

--- typeidea/blog/admin.py
+++ typeidea/blog/admin.py
@@ -18,6 +18,7 @@ class CategoryAdmin(admin.ModelAdmin):
 
     def post_count(self, obj):
         return obj.post_set.count()
+
     post_count.short_description = '文章数量'
 
 
@@ -33,7 +34,8 @@ class TagAdmin(admin.ModelAdmin):
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['title', 'category', 'status', 'created_time', 'operator']
+    list_display = ['title', 'category', 'status',
+                    'created_time', 'owner', 'operator']
     list_display_links = []
 
     list_filter = ['category', ]
@@ -55,13 +57,10 @@ class PostAdmin(admin.ModelAdmin):
 
     def operator(self, obj):
         return format_html('<a href="{}">编辑</a>',
-                           reverse('admin:blog_post_change', args=(obj.id)))
+                           reverse('admin:blog_post_change', args=(obj.id, )))
 
     operator.short_description = '操作'
 
     def save_model(self, request, obj, form, change):
         obj.owner = request.user
         return super(PostAdmin, self).save_model(request, obj, form, change)
-
-
-
