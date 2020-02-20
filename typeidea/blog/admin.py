e701a204084b569155bcd0d999bbcbb7febeb2c8
xbc <1594636097@qq.com>
2020-02-20 11:31:02 +0800

    commit admin.py

--- typeidea/blog/admin.py
+++ typeidea/blog/admin.py
@@ -34,6 +34,7 @@ class TagAdmin(admin.ModelAdmin):
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
+    exclude = ('owner',)
     list_display = ['title', 'category', 'status',
                     'created_time', 'owner', 'operator']
     list_display_links = []
@@ -47,20 +48,57 @@ class PostAdmin(admin.ModelAdmin):
     # 编辑页面
     save_on_top = True
 
-    fields = (
-        ('category', 'title'),
-        'desc',
-        'status',
-        'content',
-        'tag'
+    fieldsets = (
+        ('基础配置', {
+            'description': "基础配置描述",
+            'fields': (
+                ('title', 'category'),
+                'status'
+            ),
+        }),
+        ('内容', {
+            'fields': (
+                'desc',
+                'content'
+            ),
+        }),
+        ('额外信息', {
+            'classes': ('collapse'),
+            'fields': ('tag',),
+        })
     )
 
     def operator(self, obj):
         return format_html('<a href="{}">编辑</a>',
-                           reverse('admin:blog_post_change', args=(obj.id, )))
+                           reverse('admin:blog_post_change', args=(obj.id,)))
 
     operator.short_description = '操作'
 
     def save_model(self, request, obj, form, change):
         obj.owner = request.user
         return super(PostAdmin, self).save_model(request, obj, form, change)
+
+    def get_queryset(self, request):
+        qs = super(PostAdmin, self).get_queryset(request)
+        return qs.filter(owner=request.user)
+
+    class Media:
+        css = {
+            'all': ('https://cdn.bootcss.com/bootstrap/4.0.0-beta.2.css/bootstrap.min.css',),
+        }
+        js = ('https://cdn.bootcss.com/bootstrap/4.0.0-beta.2/js/bootstrap.bundle.js',)
+
+
+class CategoryOwnerFilter(admin.SimpleListFilter):
+    """自定义过滤器只展示当前用户分类"""
+    title = '分类过滤器'
+    parameter_name = 'owner_category'
+
+    def lookups(self, request, model_admin):
+        return Category.objects.filter(owner=request.name).values_list('id', 'name')
+
+    def queryset(self, request, queryset):
+        category_id = self.value()
+        if category_id:
+            return queryset.filter(category_id=self.value())
+        return queryset
